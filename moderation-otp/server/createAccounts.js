878ce60a5ea2400a40eb8607b58ad6e0db1434c4
TomCZQ <thomasdevpro38@gmail.com>
2024-05-27 12:37:01 +0200

    feat: -added remote database

--- moderation-otp/server/createAccounts.js
+++ moderation-otp/server/createAccounts.js
@@ -33,7 +33,7 @@ const createAccounts = async () => {
           continue;
         }
 
-        const password = generatePassword(12); // Générer un mot de passe de 12 caractères
+        const password = generatePassword(15);
         const hashedPassword = await bcrypt.hash(password, 10);
 
         const userJson = {
@@ -42,41 +42,35 @@ const createAccounts = async () => {
           id: member.user.id,
           avatar: `https://cdn.discordapp.com/avatars/${member.user.id}/${member.user.avatar}.png`,
           roles: member.roles,
-          nick: member.nick || member.user.username, // Utiliser username si nick est null
+          nick: member.nick || member.user.username,
         };
 
-        // Mettez à jour ou créez un nouvel utilisateur dans la base de données
         await User.findOneAndUpdate({ id: member.user.id }, userJson, {
           upsert: true,
           new: true,
         });
 
-        // Ajouter l'utilisateur créé à la liste des utilisateurs créés
         createdUsers.push({
           username: member.user.username,
           password: password,
         });
 
-        // Log the username and password in the console
         console.log(
           `Account created/updated for user: ${member.user.username}`
         );
         console.log(`Username: ${member.user.username}`);
-        console.log(`Password: ${password}`); // Afficher le mot de passe en clair dans la console
+        console.log(`Password: ${password}`);
       }
     }
 
-    // Créer un fichier JSON avec tous les utilisateurs et leurs mots de passe en clair
     const outputFilePath = path.join(__dirname, "created_users.json");
     fs.writeFileSync(outputFilePath, JSON.stringify(createdUsers, null, 2));
     console.log(`Created users saved to ${outputFilePath}`);
 
-    // Déconnectez-vous de la base de données
     await mongoose.disconnect();
   } catch (error) {
     console.error("Error creating accounts:", error);
   }
 };
 
-// Exécutez la fonction createAccounts
 createAccounts();
