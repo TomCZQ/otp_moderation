5130be96ab1c2da42de6b081521612c3d067aed7
TomCZQ <thomasdevpro38@gmail.com>
2024-06-02 13:55:00 +0200

    fix: added .js on paths

--- moderation-otp/server/routes/userRoutes.js
+++ moderation-otp/server/routes/userRoutes.js
@@ -3,10 +3,9 @@ const router = express.Router();
 const userController = require("../controllers/userController");
 const discordController = require("../controllers/discordController");
 const { authenticateToken } = require("../middleware/auth");
-const { loginUserValidator } = require("../validator/User"); // Le nom du fichier est sensible à la casse
-
-const { runValidation } = require("../validator/Index");
-const { disposValidator } = require("../validator/Dispos");
+const { loginUserValidator } = require("../validator/User.js"); // Le nom du fichier est sensible à la casse
+const { runValidation } = require("../validator/index.js");
+const { disposValidator } = require("../validator/Dispos.js");
 
 router.get("/members", discordController.getMembers);
 
