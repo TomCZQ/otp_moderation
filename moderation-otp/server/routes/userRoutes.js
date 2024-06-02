332626570a977bfab58e037ced6d5025fff8212d
TomCZQ <thomasdevpro38@gmail.com>
2024-06-02 15:20:48 +0200

    removed validators

--- moderation-otp/server/routes/userRoutes.js
+++ moderation-otp/server/routes/userRoutes.js
@@ -3,22 +3,18 @@ const router = express.Router();
 const userController = require("../controllers/userController");
 const discordController = require("../controllers/discordController");
 const { authenticateToken } = require("../middleware/auth");
-//const { loginUserValidator } = require("../validator/User"); // Le nom du fichier est sensible à la casse
-//const { runValidation } = require("../validator/index");
-//const { disposValidator } = require("../validator/Dispos");
 
 router.get("/members", discordController.getMembers);
 
 router.post(
   "/login",
-  //loginUserValidator,
-  //runValidation,
+
   userController.loginUser
 );
 
 router.get(
   "/me",
-  //disposValidator,
+
   authenticateToken,
   userController.getUserDetails
 );
