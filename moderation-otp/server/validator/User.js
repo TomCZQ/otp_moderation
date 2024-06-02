30a882a1e8eeb07414d7f73d30f6e99d93a0ef9a
TomCZQ <thomasdevpro38@gmail.com>
2024-06-02 14:52:58 +0200

    fix : another path

--- moderation-otp/server/validator/User.js
+++ moderation-otp/server/validator/User.js
@@ -10,11 +10,3 @@ exports.loginUserValidator = [
     .isEmpty()
     .withMessage("Le mot de passe c'est pas une option hein"),
 ];
-
-exports.runValidation = (req, res, next) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
-  }
-  next();
-};
